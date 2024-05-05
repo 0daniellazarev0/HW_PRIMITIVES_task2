fc283484f6384f480b053ebd41488a2935231583
Daniel Lazarev <daniil.lt@hotmail.com>
2024-05-06 00:35:27 +0300

    Code reformatted

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
-int StartBalance = 100;
-int TopupAmount = 2000;
-int EndBalance = TopupAmount > 1_000 ? StartBalance + TopupAmount + TopupAmount / 100 : StartBalance + TopupAmount;
+        int StartBalance = 100;
+        int TopupAmount = 2_000;
+        int EndBalance = (TopupAmount > 1_000) ? (StartBalance + TopupAmount + (TopupAmount / 100)) : (StartBalance + TopupAmount);
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
-System.out.println("Благодарим за пополнение счета! Ваш баланс: ₽" + EndBalance);
+        System.out.println("Благодарим за пополнение счета! Ваш баланс: ₽" + EndBalance);
     }
 }
